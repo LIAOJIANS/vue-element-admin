e8476224064370dc1c710dcd88b66341a97d49bd
LIAOJIANS <272781702@qq.com>
2020-06-18 14:48:47 +0800

    更新

---
+++ src/router/index.js
@@ -0,0 +1,95 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+/* Layout */
+import Layout from '@/layout'
+
+export const constantRoutes = [
+  {
+    path: '/redirect',
+    component: Layout,
+    hidden: true,
+    children: [
+      {
+        path: '/redirect/:path(.*)',
+        component: () => import('@/views/redir' +
+      '.' +
+      'ect/index')
+      }
+    ]
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+  {
+    path: '/404',
+    component: () => import('@/views/error-page/404'),
+    hidden: true
+  },
+  {
+    path: '/401',
+    component: () => import('@/views/error-page/401'),
+    hidden: true
+  },
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        component: () => import('@/views/dashboard/index'),
+        name: 'Dashboard',
+        meta: { title: 'Dashboard', icon: 'dashboard', affix: true }
+      }
+    ]
+  }
+]
+
+export const asyncRoutes = [
+  {
+    path: '/book',
+    redirect: '/book/create',
+    meta: {
+      title: '图书管理',
+      icon: '404'
+    },
+    component: Layout,
+    children: [
+      {
+        path: '/book/create',
+        component: () => import('@/views/book/createBook'),
+        meta: { title: '图书管理', icon: 'dashboard'}
+      }
+    ]
+  },
+  {
+    path: '*',
+    component: () => import('@/views/error-page/404'),
+    hidden: true,
+    meta: {
+      title: 'Error Pages',
+      icon: '404'
+    }
+  }
+]
+
+const createRouter = () => new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
+})
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router
